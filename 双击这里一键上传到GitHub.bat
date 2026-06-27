902bab32c818f4c4c52dff8db802c723becc04f3
hongqitengda <yaojj0412@outlook.com>
2026-06-27 21:50:26 +0800

    Revert "Upgrade website to V19.2"
    This reverts commit 59bad466e35565004b720659339c4c69a75c5cca.

--- 双击这里一键上传到GitHub.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-set "TMPPS=%TEMP%\hqt-deploy-%RANDOM%-%RANDOM%.ps1"
-copy /Y "%~dp0deploy.ps1" "%TMPPS%" >nul
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%TMPPS%" -PackageDir "%~dp0"
-set "ERR=%ERRORLEVEL%"
-del /Q "%TMPPS%" >nul 2>&1
-exit /b %ERR%
